54b62efe0a610a61c22acf49ef90e14964096d32
demonflowne <ibuemmanuel60@gmail.com>
2024-12-21 03:31:01 +0100

    First commit

---
+++ frontend/src/components/Users.tsx
@@ -0,0 +1,48 @@
+import { isAxiosError } from "axios";
+import { useState, useEffect } from "react";
+import axios from "../api/axios";
+
+type User = {
+  username: string;
+};
+
+const Users = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+    const getUsers = async () => {
+      try {
+        const response = await axios.get("/users", {
+          signal: controller.signal,
+        });
+        console.log(response.data);
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        isMounted && setUsers(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getUsers();
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, []);
+  return (
+    <article>
+      <h2>Users List</h2>
+      {users.length ? (
+        <ul>
+          {users.map((user, i) => (
+            <li key={i}>{user.username}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No users to display</p>
+      )}
+    </article>
+  );
+};
+
+export default Users;
