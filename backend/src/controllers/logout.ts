4ec00190e9a456336f9aa1e144d52b541a4d9c0d
demonflowne <ibuemmanuel60@gmail.com>
2024-12-26 10:51:23 +0100

    set up logout cookies

--- backend/src/controllers/logout.ts
+++ backend/src/controllers/logout.ts
@@ -6,18 +6,18 @@ export async function logout(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
 ): Promise<void> {
   const cookies = req.cookies;
-  console.log({ cookies }, "haha");
   // Token was never there anyway lol
-  if (!cookies?.jwt) {
+  if (!cookies?.ticket) {
     res.sendStatus(204);
     return;
   }
-  console.log(cookies);
-  res.clearCookie("ticket", {
-    httpOnly: true,
-    sameSite: "strict",
-    // secure: true ,    taking it out for thunder client ,has issue with it
-    // secure: true
-  }); // secure: true = only serves on http
+
+  res.clearCookie("ticket");
+  // , {
+  //   httpOnly: true,
+  //   sameSite: "strict"
+  //   // secure: true ,    taking it out for thunder client ,has issue with it
+  //   // secure: true
+  // }); // secure: true = only serves on http
   res.sendStatus(204);
 }
